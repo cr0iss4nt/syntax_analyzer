96d0f7b5464433759daaf2a2f8ab48c13ef00c10
cr0iss4nt <809dan4ik@gmail.com>
2026-04-02 18:23:25 +0300

    Added semantic analysis and hideable syntactic elements

--- modules/analyzer.py
+++ modules/analyzer.py
@@ -4,10 +4,10 @@ import os
 load_dotenv()
 
 import spacy
-model = os.getenv("SPACY_MODEL")
+model = os.getenv("SPACY_MODEL", "ru_core_news_sm")
 try:
     nlp = spacy.load(model)
-except:
+except Exception:
     import spacy.cli
     spacy.cli.download(model)
     nlp = spacy.load(model)
@@ -17,5 +17,7 @@ from spacy import displacy
 def analyze(text):
     doc = nlp(text)
     svgs = [displacy.render(i, style="dep", jupyter=False) for i in doc.sents]
-    return [(token.text, token.pos_, token.dep_, token.head.text, token.head.pos_) for token in doc], svgs
+    syntactic_analysis = [(token.text, token.pos_, token.dep_, token.head.text, token.head.pos_) for token in doc]
+    semantic_analysis = [(entity.text, entity.label_) for entity in doc.ents]
+    return syntactic_analysis, svgs, semantic_analysis
 
